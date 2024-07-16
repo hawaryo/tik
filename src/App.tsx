794e2aec480ea875328e207f28f5340bc31d8e4c
hawaryo <skarmar50@gmail.com>
2024-07-16 21:58:10 +0300

    initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Square } from "../src/components/x.tsx";
+
+function App() {
+  return (
+    <div>
+      <Square />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/x.tsx
@@ -0,0 +1,4 @@
+import styles from "../style/x.module.css";
+export function Square() {
+  return <button className={styles.xbutton}>x</button>;
+}
